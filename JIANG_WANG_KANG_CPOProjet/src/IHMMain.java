f1901f06d0dafeeb8dcc0e0edebe3e408c6165d9
Marshellson <jiangyilun2000@outlook.com>
2021-02-07 20:14:04 +0100

    Modifier IHM_Page_Principe

--- JIANG_WANG_KANG_CPOProjet/src/IHMMain.java
+++ JIANG_WANG_KANG_CPOProjet/src/IHMMain.java
@@ -1,2 +1,26 @@
-public class IHMMain {
+import javax.swing.*;
+
+public class IHMMain extends javax.swing.JFrame {
+    private JPanel main_interface;
+    private JButton searchButton;
+    private JTextField textField1;
+    private JButton sortButton;
+    private JButton addFilmButton;
+    private JLabel lesFilmsLabel;
+    private JList list1;
+    private JTextArea nihaomaTextArea;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("IHMMain");
+        frame.setContentPane(new IHMMain().main_interface);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+    }
 }
---
+++ JIANG_WANG_KANG_CPOProjet/src/main.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class main {
+    public static void main(String[] args) {
+    }
+}
