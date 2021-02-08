bbcac1b44c7164d62d8b539a153840ad9294dbcd
Marshellson <jiangyilun2000@outlook.com>
2021-02-08 09:56:47 +0100

    just for test

--- JIANG_WANG_KANG_CPOProjet/src/IHMMain.java
+++ JIANG_WANG_KANG_CPOProjet/src/IHMMain.java
@@ -7,10 +7,23 @@ public class IHMMain extends javax.swing.JFrame {
     private JButton sortButton;
     private JButton addFilmButton;
     private JLabel lesFilmsLabel;
-    private JList lesfilmsList;
+    public JList lesfilmsList;
     private JTextArea filmInfoTextArea;
     private JLabel nameOfFilmLabel;
 
+
+    public IHMMain(){
+        DefaultListModel<String> l1 = new DefaultListModel<>();
+        l1.addElement("Item1");
+        l1.addElement("Item2");
+        l1.addElement("Item3");
+        l1.addElement("Item4");
+        lesfilmsList = new JList(l1);
+        lesfilmsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        lesfilmsList.setSelectedIndex(0);
+
+
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("IHMMain");
