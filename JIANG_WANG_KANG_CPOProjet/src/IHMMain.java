cfa78029e8415df0f25e4fdd8a1650cd6eb480ce
Marshellson <jiangyilun2000@outlook.com>
2021-02-09 13:54:59 +0100

    add setmodel for JList lesfilmsList

--- JIANG_WANG_KANG_CPOProjet/src/IHMMain.java
+++ JIANG_WANG_KANG_CPOProjet/src/IHMMain.java
@@ -7,23 +7,23 @@ public class IHMMain extends javax.swing.JFrame {
     private JButton sortButton;
     private JButton addFilmButton;
     private JLabel lesFilmsLabel;
-    public JList lesfilmsList;
+    private JList<lesfilms> lesfilmsList;
     private JTextArea filmInfoTextArea;
     private JLabel nameOfFilmLabel;
 
+    DefaultListModel<lesfilms> listModel = new DefaultListModel<>();
 
-    public IHMMain(){
-        DefaultListModel<String> l1 = new DefaultListModel<>();
-        l1.addElement("Item1");
-        l1.addElement("Item2");
-        l1.addElement("Item3");
-        l1.addElement("Item4");
-        lesfilmsList = new JList(l1);
-        lesfilmsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        lesfilmsList.setSelectedIndex(0);
+    public IHMMain() {
+        lesfilmsList.setModel(listModel);
+        listModel.addElement(new lesfilms("nihaoma", "DVD"));
+        listModel.addElement(new lesfilms("Your name", "B-Ray"));
+        listModel.addElement(new lesfilms("nihaoma", "DVD"));
+        listModel.addElement(new lesfilms("Your name", "B-Ray"));
+
 
 
     }
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("IHMMain");
@@ -31,8 +31,41 @@ public class IHMMain extends javax.swing.JFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        SwingUtilities.invokeLater(IHMMain::new);
+
     }
 
+    private class lesfilms {
+        String nomdefilm;
+        String model; //This model here is to define if the movir is stocked in DVD or B-ray
+        // TODO: Change model after when other disagree......
+
+        public lesfilms(String nomdefilm, String model) {
+            this.nomdefilm = nomdefilm;
+            this.model = model;
+        }
+
+        public String getNomdefilm() {
+            return nomdefilm;
+        }
+
+        public void setNomdefilm(String nomdefilm) {
+            this.nomdefilm = nomdefilm;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        @Override
+        public String toString() {
+            return nomdefilm + "\n" + model;
+        }
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
